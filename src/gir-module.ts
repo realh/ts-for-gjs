3d1903a9b401c374fc20efdc6fe7dea52979372c
Tony Houghton <h@realh.co.uk>
2020-10-22 15:36:14 +0100

    Fixed localName()

--- src/gir-module.ts
+++ src/gir-module.ts
@@ -961,6 +961,8 @@ export class GirModule {
             name = this.transformation.transformClassName(name.$.name)
             if (mod === this)
                 return name
+            else
+                name = mod.name + '.' + name
         }
         if (name.indexOf('.') < 0)
             return name
@@ -1391,9 +1393,12 @@ export class GirModule {
     }
 
     private processSelfAndInterfaceSignals(cls: GirClass, propertyNames: string[]): string[] {
+        const dolog = cls._fullSymName == 'Gtk.ArrowAccessible'
         const signals: string[] = []
         this.forEachInterfaceAndSelf(cls, (e) => {
             const clsName = this.localName(e)
+            if (dolog)
+                console.log(`Local name of ${e._fullSymName} in ${this.name} is ${clsName}`) 
             signals.push(...this.processSignals(e, clsName))
             signals.push(...this.generateNotifyMethods(e, propertyNames, clsName))
         })
